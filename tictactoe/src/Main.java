f20f882b32fa1934f6d41a5fa6339ef4342a3ed2
Hashmi-dev <ahmadhashmi04@outlook.com>
2024-09-18 14:54:22 +0300

    Add files via upload

---
+++ tictactoe/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+// instance of tictactoe class
+        Tictactoe tictactoe = new Tictactoe();
+//        TicTacToe_New ticTacToe_new = new TicTacToe_New();
+
+    }
+}
